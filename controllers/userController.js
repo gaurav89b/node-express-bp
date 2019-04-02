3c949cb77ae92c45cb2e9b35cba7fad3eacd66ac
Gaurav Bakshi <gaurav.bakshi@studiographene.com>
2019-04-02 13:51:29 +0530

    added controller and changes in routes file

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+/**
+ * User specific routes method goes here.
+ * 
+ */
+// Display list of all Genre.
+let User = {};
+User.list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre list');
+};
+
+User.detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Genre detail');
+};
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// required Contoller
+var user_controller = require('../controllers/userController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.send("Hello World New");
 });
+
+router.get('/users', user_controller.list);
+router.get('/users/detail', user_controller.detail);
 
 module.exports = router;
